d87fc9ebbd28c28197f25c3df531bc3f24dd2fdf
PeyTheCrafter <peythecrafter@gmail.com>
2018-03-20 10:14:31 +0100

    Actualizaciones

--- src/modelo/Tablero.java
+++ src/modelo/Tablero.java
@@ -4,6 +4,21 @@ import control.Iniciable;
 
 public class Tablero implements Iniciable {
 
+	protected Carta tablero[][];
+
+	public Tablero(Carta[][] tablero) {
+		super();
+		this.tablero = tablero;
+	}
+
+	public Carta[][] getTablero() {
+		return tablero;
+	}
+
+	public void setTablero(Carta[][] tablero) {
+		this.tablero = tablero;
+	}
+
 	@Override
 	public void desordenarTablero() {
 		// TODO Auto-generated method stub
