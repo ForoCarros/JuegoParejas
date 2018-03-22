54893217e90cfe5772fdada3b57e848461b0005b
PeyTheCrafter <peythecrafter@gmail.com>
2018-03-22 09:57:11 +0100

    Añadida propiedad emparejada

--- src/modelo/Carta.java
+++ src/modelo/Carta.java
@@ -1,25 +1,38 @@
 package modelo;
 
 public class Carta {
-	
-private int id;
-private boolean velada=false;
 
-public Carta(int id) {
-	super();
-	this.id = id;
-}
-public int getId() {
-	return id;
-}
-public void setId(int id) {
-	this.id = id;
-}
-public boolean isVelada() {
-	return velada;
-}
-public void setVelada(boolean velada) {
-	this.velada = velada;
-}
+	private int id;
+	private boolean velada = false;
+	private boolean emparejada = false;
+
+	public Carta(int id) {
+		super();
+		this.id = id;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public boolean isVelada() {
+		return velada;
+	}
+
+	public void setVelada(boolean velada) {
+		this.velada = velada;
+	}
+
+	public boolean isEmparejada() {
+		return emparejada;
+	}
+
+	public void setEmparejada(boolean emparejada) {
+		this.emparejada = emparejada;
+	}
 
 }
