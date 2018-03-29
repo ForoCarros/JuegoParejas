d680e49be0eac53b29590d9f7856e15e46621b49
BuildTools <unconfigured@null.spigotmc.org>
2018-03-29 23:24:32 +0200

    Cambiadas las clases necesarias.

--- src/control/MyActionListener.java
+++ src/control/MyActionListener.java
@@ -1,6 +1,5 @@
 package control;
 
-
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -9,19 +8,23 @@ import javax.swing.JButton;
 public class MyActionListener implements ActionListener {
 
 	Accion accion;
-	
+	ParaUI paraUI;
+
 	// Que traigo el paraui aqui para ver en el tablero las ids de las cartas????
 
-	public MyActionListener(Accion accion) {
+	public MyActionListener(ParaUI paraUI) {
 		super();
-		this.accion = accion;
+		this.accion = new Accion();
+		this.paraUI = paraUI;
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		assert e == null : "objeto nulo";
 		JButton boton = (JButton) e.getSource();
-		String[]coordenadas = boton.getName().split("-");
+		String[] coordenadas = boton.getName().split("-");
 		accion.realizarJugada(coordenadas);
+		System.out.println(coordenadas[0] + " " + coordenadas[1]);
+		this.paraUI.actualizarVentana();
 	}
 }
