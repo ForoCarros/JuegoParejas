ebfb75eb7aaaa4186a57ce612f7c137defee6029
BuildTools <unconfigured@null.spigotmc.org>
2018-04-01 20:20:45 +0200

    Asserts al revés.

--- src/control/MyActionListener.java
+++ src/control/MyActionListener.java
@@ -21,7 +21,7 @@ public class MyActionListener implements ActionListener {
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		assert e == null : "objeto nulo";
+		assert e != null : "objeto nulo";
 		JButton boton = (JButton) e.getSource();
 		String[] coordenadas = boton.getName().split("-");
 		accion.realizarJugada(coordenadas);
