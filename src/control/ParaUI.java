360bc1f946286e20d24774aa8a8fff60cc57c127
BuildTools <unconfigured@null.spigotmc.org>
2018-04-01 17:53:04 +0200

    Ahora se muestran las imágenes, no los ids

--- src/control/ParaUI.java
+++ src/control/ParaUI.java
@@ -1,6 +1,7 @@
 package control;
 
 import modelo.Tablero;
+import utiles.StretchIcon;
 import vista.UIParejas;
 
 public class ParaUI extends UIParejas implements ParaUIable {
@@ -8,6 +9,9 @@ public class ParaUI extends UIParejas implements ParaUIable {
 	MyActionListener listener;
 	Juego juego;
 	final int LADO = 6;
+	final int ARCHIVOS = 20;
+	final String EXTENSION = ".jpg";
+	StretchIcon iconos[];
 
 	public ParaUI() {
 		super();
@@ -15,8 +19,16 @@ public class ParaUI extends UIParejas implements ParaUIable {
 		this.juego = new Juego(this.tablero);
 		this.listener = new MyActionListener(this, this.juego);
 		this.botonera.iniciarBotonera(this.LADO);
+		this.generarImagenes();
 		this.establecerListeners();
 		this.actualizarVentana();
+	}
+
+	private void generarImagenes() {
+		this.iconos = new StretchIcon[this.ARCHIVOS];
+		for (int i = 0; i < this.ARCHIVOS; i++) {
+			this.iconos[i] = new StretchIcon("assets/" + i + this.EXTENSION);
+		}
 	}
 
 	private void establecerListeners() {
@@ -29,32 +41,15 @@ public class ParaUI extends UIParejas implements ParaUIable {
 
 	@Override
 	public void actualizarVentana() {
-		this.debug();
 		for (int i = 0; i < this.botonera.botones.length; i++) {
 			for (int j = 0; j < this.botonera.botones[i].length; j++) {
 				if (!this.tablero.getCartas()[i][j].isVelada() || this.tablero.getCartas()[i][j].isEmparejada()) {
-					this.botonera.botones[i][j].setText(String.valueOf(this.tablero.getCartas()[i][j].getId()));
+					// this.botonera.botones[i][j].setText(String.valueOf(this.tablero.getCartas()[i][j].getId()));
+					this.botonera.botones[i][j].setIcon(this.iconos[this.tablero.getCartas()[i][j].getId()]);
 				} else {
-					this.botonera.botones[i][j].setText("");
+					this.botonera.botones[i][j].setIcon(null);
 				}
 			}
-		}
-	}
-
-	private void debug() {
-		for (int i = 0; i < this.tablero.getCartas().length; i++) {
-			for (int j = 0; j < this.tablero.getCartas()[i].length; j++) {
-				System.out.print(
-						this.tablero.getCartas()[i][j].getId() < 10 ? this.tablero.getCartas()[i][j].getId() + " "
-								: this.tablero.getCartas()[i][j].getId());
-			}
-			System.out.println();
-		}
-		for (int i = 0; i < this.tablero.getCartas().length; i++) {
-			for (int j = 0; j < this.tablero.getCartas()[i].length; j++) {
-				System.out.print(this.tablero.getCartas()[i][j].isEmparejada() ? "E" : "·");
-			}
-			System.out.println();
 		}
 	}
 }
